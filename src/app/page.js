58fcacc88ebbc506131da20b54c5abfeef5c3266
julsCadenas <jscadenas12@gmail.com>
2024-04-25 23:07:50 +0800

    initial commit

---
+++ src/app/page.js
@@ -0,0 +1,30 @@
+"use client";
+import React from "react";
+import Form from "@/src/components/Form";
+import Header from "@/src/components/Header";
+import TODOHero from "@/src/components/TODOHero";
+import TODOList from "@/src/components/TODOList";
+
+function Home() {
+  const [todos, setTodos] = React.useState([]);
+  React.useEffect(() => {
+    const storedTodos = localStorage.getItem("todos");
+    if (storedTodos) {
+      setTodos(JSON.parse(storedTodos));
+    }
+  }, []);
+  const todos_completed = todos.filter(
+    (todo) => todo.is_completed === true
+  ).length;
+  const total_todos = todos.length;
+  
+  return (
+    <div className="wrapper">
+      <Header />
+      <TODOHero todos_completed={todos_completed} total_todos={total_todos} />
+      <Form todos={todos} setTodos={setTodos} />
+      <TODOList todos={todos} setTodos={setTodos} />
+    </div>
+  );
+}
+export default Home;
